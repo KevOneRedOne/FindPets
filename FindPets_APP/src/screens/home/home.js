029e349bf51cac46d109e43097c9599afac7dbb0
KevOneRedOne <kevinalves@hotmail.fr>
2023-01-08 12:07:36 +0100

    feat: Update Home page

---
+++ FindPets_APP/src/screens/home/home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import BottomEndpoints from '../../config/bottom_endpoints';
+
+const Home = () => {
+    return (
+        <BottomEndpoints/>
+    );
+};
+
+export default Home;
